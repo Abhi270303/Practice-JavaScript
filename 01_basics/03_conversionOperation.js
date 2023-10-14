4a441ed25e418d90d9c71f069cd47b0193ea648f
Abhishek yadav <barrelabhi45@gmail.com>
2023-10-14 12:18:26 +0530

    Conversion Understood

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Abhishek"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Abhishek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhishek" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+

c3f94b37749652687c9663f975e7bd9623afeeb5
Abhishek yadav <barrelabhi45@gmail.com>
2023-10-14 21:15:51 +0530

    01_arrays understood

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+//shallow copy is a copy whose properties share the same reference like heap in memory
+//Deep copy is a copy whose properties don't share the same refrence like stack in memory
+
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["Ironman", "Thor"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+//console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+
+//Slice does not manipulate original array
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+//splice manipulate original array
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);

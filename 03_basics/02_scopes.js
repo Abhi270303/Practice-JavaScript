f5b208b9fb48bf5858cfb2229986220ae061d54c
Abhishek yadav <barrelabhi45@gmail.com>
2023-10-15 13:05:54 +0530

    scope level and mini hoisting understood

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,55 @@ let a = 300
 if (true) { //block scope
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Abhishek"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Abhishek"
+    if (username === "Abhishek") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5)) // function ko declaration ke pehle access kar sakte isme
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5) // function ko declaration ke pehle access nahi kar sakte isme
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5))
